72d1e0f4325c4df3fe64827f2351d706d7d70ada
github username <github email address>
2020-09-03 15:45:06 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+
+    def index
+        @product = (params[:product]) 
+
+    cart = session[:cart] || []
+    cart << @product
+
+    session[:cart] = cart
+    @cart = session[:cart]
+    end
+    
+    def add
+    end 
+end
